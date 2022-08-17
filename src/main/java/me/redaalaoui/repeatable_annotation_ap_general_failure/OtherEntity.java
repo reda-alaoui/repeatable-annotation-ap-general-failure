49617847b9e41241ca9b158c7f24205f4e4495c6
Réda Housni Alaoui <reda-alaoui@hey.com>
2022-08-17 16:32:42 +0200

    Add OtherEntity

---
+++ src/main/java/me/redaalaoui/repeatable_annotation_ap_general_failure/OtherEntity.java
@@ -0,0 +1,10 @@
+package me.redaalaoui.repeatable_annotation_ap_general_failure;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Réda Housni Alaoui
+ */
+@Entity
+public class OtherEntity {
+}
